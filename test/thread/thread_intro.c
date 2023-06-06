6321aee215f080e00b48f921542ab769449e8fe8
Akadil <akadil.kalimoldayev@gmail.com>
2023-06-06 18:24:38 +0200

    Research period finished

---
+++ test/thread/thread_intro.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+
+void	*routine()
+{
+	printf("Hello World!\n");
+	sleep(2);
+	printf("My name is Akadil\n");
+	return (NULL);
+}
+
+/**
+ * @brief The video from Codevault "Short introduction to threads (pthreads)"
+ */
+int	main(void)
+{
+	pthread_t	t1;
+	pthread_t	t2;
+
+	pthread_create(&t1, NULL, &routine, NULL);
+	pthread_create(&t2, NULL, &routine, NULL);
+	pthread_join(t1, NULL);
+	pthread_join(t2, NULL);
+
+	return (0);
+}
