aca78d13358de701027f4f0257d26f877a56c6cc
akadil.kalimoldayev@gmail.com <akadil.kalimoldayev@gmail.com>
2023-06-30 14:47:23 +0200

    Added ft_is_my_turn

--- srcs/routine/ft_routine.c
+++ srcs/routine/ft_routine.c
@@ -6,15 +6,18 @@
 /*   By: akalimol <akalimol@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/20 14:29:54 by akalimol          #+#    #+#             */
-/*   Updated: 2023/06/27 20:29:22 by akalimol         ###   ########.fr       */
+/*   Updated: 2023/06/28 14:16:06 by akalimol         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/ft_routine.h"
+#include <stdio.h>
+#include <pthread.h>
+#include <unistd.h>
 
 int	ft_is_my_turn2(t_philo *philo)
 {
-	if(philo->rules->num_philo % 2 == 0)
+	if (philo->rules->num_philo % 2 == 0)
 	{
 		if (philo->num % 2 == 0)
 			return (1);
@@ -24,7 +27,7 @@ int	ft_is_my_turn2(t_philo *philo)
 		return (1);
 	philo->iter = philo->rules->num_philo / 2;
 	philo->host = philo->num_meal % philo->rules->num_philo;
-	while(philo->iter != 0)
+	while (philo->iter != 0)
 	{
 		if (philo->num % philo->rules->num_philo == philo->host)
 			return (1);
@@ -36,43 +39,49 @@ int	ft_is_my_turn2(t_philo *philo)
 	return (0);
 }
 
-void    ft_unlock_forks(t_philo *philo)
+void	ft_unlock_forks(t_philo *philo)
 {
-	if (ft_is_my_turn2(philo))
-	{
-		pthread_mutex_unlock(&philo->next->fork);
-		pthread_mutex_unlock(&philo->fork);
-	}
-	else
-	{
-		pthread_mutex_unlock(&philo->fork);
-		pthread_mutex_unlock(&philo->next->fork);
-	}
+	pthread_mutex_unlock(&philo->next->fork);
+	pthread_mutex_unlock(&philo->fork);
 }
 
-void    *ft_routine(void *arg)
+void	ft_status(t_philo *philo)
 {
-    t_philo *philo;
+	pthread_mutex_lock(&philo->exit);
+	philo->exit_code = -4;
+	pthread_mutex_unlock(&philo->exit);
+}
 
-    philo = (t_philo *)arg;
-    if (ft_start_schedule(philo) != 0)
-        return (ft_handle_error(philo), NULL);
-    while (1)
-    {
-        if (ft_fork(philo) != 0)
-            break ;
-        if (ft_eat(philo) != 0)
-        {
-            ft_unlock_forks(philo);
-            break ;
-        }
-        ft_unlock_forks(philo);
-        if (--philo->success == 0)
-            break ; 
-        if (ft_sleep(philo) != 0)
-            break ;
-    }
-    if (philo->exit_code == -1 || philo->exit_code == -2)
-        ft_handle_error(philo);
-    return (NULL);
+void	ft_one_philo(t_philo *philo)
+{
+	usleep(philo->rules->time_die * 1000 + 1000);
+}
+
+void	*ft_routine(void *arg)
+{
+	t_philo	*philo;
+
+	philo = (t_philo *)arg;
+	if (ft_start_schedule(philo) != 0)
+		return (ft_handle_error(philo), NULL);
+	if (philo->rules->num_philo == 1)
+		return (ft_one_philo(philo), NULL);
+	while (1)
+	{
+		if (ft_fork(philo) != 0)
+			break ;
+		if (ft_eat(philo) != 0)
+		{
+			ft_unlock_forks(philo);
+			break ;
+		}
+		ft_unlock_forks(philo);
+		if (--philo->success == 0)
+			break ;
+		if (ft_sleep(philo) != 0)
+			break ;
+	}
+	if (philo->exit_code == -2)
+		ft_handle_error(philo);
+	return (ft_status(philo), NULL);
 }
