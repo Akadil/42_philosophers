d31fd22e7fe0bfe62e5685335020010deaf40e71
akadil.kalimoldayev@gmail.com <akadil.kalimoldayev@gmail.com>
2023-06-30 14:45:18 +0200

    Norminette in philosophers

--- libft/srcs/linked_list/ft_lstfirst.c
+++ libft/srcs/linked_list/ft_lstfirst.c
@@ -6,14 +6,14 @@
 /*   By: akalimol <akalimol@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/20 14:18:35 by akalimol          #+#    #+#             */
-/*   Updated: 2023/05/20 14:18:55 by akalimol         ###   ########.fr       */
+/*   Updated: 2023/06/28 14:19:12 by akalimol         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
 #include "struct_list.h"
+#include <stdio.h>
 
-t_list  *ft_lstfirst(t_list *lst)
+t_list	*ft_lstfirst(t_list *lst)
 {
 	if (!lst)
 		return (NULL);
--- libft/srcs/other/ft_isnum.c
+++ libft/srcs/other/ft_isnum.c
@@ -3,27 +3,27 @@
 /*                                                        :::      ::::::::   */
 /*   ft_isnum.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: akadilkalimoldayev <akadilkalimoldayev@    +#+  +:+       +#+        */
+/*   By: akalimol <akalimol@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/12 16:09:25 by akadilkalim       #+#    #+#             */
-/*   Updated: 2023/06/12 16:11:18 by akadilkalim      ###   ########.fr       */
+/*   Updated: 2023/06/28 14:19:58 by akalimol         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int ft_isnum(char *str)
+int	ft_isnum(char *str)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    if (!str)
-        return (0);
-    if (str[i] == '-')
-        i++;
-    while (str[i])
-    {
-        if (str[i] > '9' || str[i] < '0')
-            return (0);
-        i++;
-    }
-    return (1);
+	i = 0;
+	if (!str)
+		return (0);
+	if (str[i] == '-')
+		i++;
+	while (str[i])
+	{
+		if (str[i] > '9' || str[i] < '0')
+			return (0);
+		i++;
+	}
+	return (1);
 }
